1b67469f72d92517ecfda82a4abf0f6df8b9855c
Arthur <artopenheimervilela@gmail.com>
2023-05-02 15:25:23 -0300

    add routes to nearest networks and nodes

--- signal_map/location/views.py
+++ signal_map/location/views.py
@@ -1,10 +1,39 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import DeviceLocation
 from .serializer import DeviceLocationSerializer
-
+from rest_framework.response import Response
+from wifimap.serializer import WifiNodeSerializer
+from wifimap.models import WifiNode
 class DeviceLocationViewSet(viewsets.ModelViewSet):
-    queryset = DeviceLocation.objects.all()
     serializer_class = DeviceLocationSerializer
+    queryset = DeviceLocation.objects.all()
     
-#criar uma nova rota pra poder retornar o caminho do ponto para o dispositivo com base no local escolhido, mas talvez fazer esse calculo
-#usando latitudes e longitudes seja mais facil, o dispositivo envia a latitude e longitude e o servidor retorna o caminho para o celular
+class NearestNetworks(generics.GenericAPIView):
+    serializer_class = DeviceLocationSerializer
+
+    def get_queryset(self):
+        device_id = self.kwargs['device_id']
+        queryset = DeviceLocation.objects.filter(device_id=device_id).order_by('distance')[:3]
+        return queryset
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        data = {
+            'results': serializer.data
+        }
+        return Response(data)
+
+class NearestWifiNodes(generics.ListAPIView):
+    serializer_class = WifiNodeSerializer
+
+    def get_queryset(self):
+        # Obter os SSIDs das três redes mais próximas
+        device_id = self.kwargs['device_id']
+        networks = DeviceLocation.objects.filter(device_id=device_id).order_by('distance')[:3]
+        ssids = [network.ssid for network in networks]
+
+        # Filtrar os nós Wi-Fi com base nos SSIDs das redes
+        queryset = WifiNode.objects.filter(ssid__in=ssids)
+
+        return queryset
--- signal_map/signal_map/urls.py
+++ signal_map/signal_map/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from wifimap.views import WifiNodeViewSet
-from location.views import DeviceLocationViewSet
+from location.views import DeviceLocationViewSet, NearestNetworks, NearestWifiNodes
+from destination.views import ChosenDestinationViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('wifinodes', WifiNodeViewSet, basename='wifinodes')
 router.register('devicel', DeviceLocationViewSet, basename='devicel')
+router.register('chosendest', ChosenDestinationViewSet, basename='chosendest')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('devicel/<int:device_id>/nearest_networks', NearestNetworks.as_view()),
+    path('devicel/<int:device_id>/nearest_nodes', NearestWifiNodes.as_view()),
 ]

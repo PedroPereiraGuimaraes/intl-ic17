9bc6b1022bac4cfae64b9b7fb313fd403aa6e5cf
Arthur <artopenheimervilela@gmail.com>
2023-05-09 12:18:04 -0300

    add triangulation

--- signal_map/location/views.py
+++ signal_map/location/views.py
@@ -1,39 +1,70 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, views
 from .models import DeviceLocation
 from .serializer import DeviceLocationSerializer
 from rest_framework.response import Response
-from wifimap.serializer import WifiNodeSerializer
-from wifimap.models import WifiNode
+from networks.models import Networks
+
 class DeviceLocationViewSet(viewsets.ModelViewSet):
     serializer_class = DeviceLocationSerializer
     queryset = DeviceLocation.objects.all()
-    
-class NearestNetworks(generics.GenericAPIView):
-    serializer_class = DeviceLocationSerializer
 
-    def get_queryset(self):
-        device_id = self.kwargs['device_id']
-        queryset = DeviceLocation.objects.filter(device_id=device_id).order_by('distance')[:3]
-        return queryset
+def triangulacao(x1, y1, distancia1, x2, y2, distancia2, x3, y3, distancia3):
+    raio1 = distancia1
+    raio2 = distancia2
+    raio3 = distancia3
 
+    ##Fazendo os calculos da triangulação por simplificação
+    a = 2 * (-x1 + x2)
+    b = 2 * (-y1 + y2)
+    c = (raio1 ** 2) - (raio2 ** 2) - (x1 ** 2) + (x2 ** 2) - (y1 ** 2) + (y2 ** 2)
+    d = 2 * (-x2 + x3)
+    e = 2 * (-y2 + y3)
+    f = (raio2 ** 2) - (raio3 ** 2) - (x2 ** 2) + (x3 ** 2) - (y2 ** 2) + (y3 ** 2)
+
+    x = 10000
+    y = 10000
+
+    ##Calculando as coordenadas de x e y do dispositivo
+    if ((e * a) - (b * d)) == 0 and ((b * d) - (a * e)) == 0:
+        x = 0
+        y = 0
+    elif((e*a) - (b*d)) == 0:
+        y = ((c * d) - (a * f)) / ((b * d) - (a * e))
+        x=0
+    elif((b * d) - (a * e)) == 0:
+        x = ((c * e) - (f * b)) / ((e * a) - (b * d))
+        y = 0
+    else:
+        x = ((c * e) - (f * b)) / ((e * a) - (b * d))
+        y = ((c * d) - (a * f)) / ((b * d) - (a * e))
+
+    return x, y
+
+class UserLocation(views.APIView):
     def get(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        data = {
-            'results': serializer.data
-        }
-        return Response(data)
-
-class NearestWifiNodes(generics.ListAPIView):
-    serializer_class = WifiNodeSerializer
-
-    def get_queryset(self):
-        # Obter os SSIDs das três redes mais próximas
         device_id = self.kwargs['device_id']
-        networks = DeviceLocation.objects.filter(device_id=device_id).order_by('distance')[:3]
-        ssids = [network.ssid for network in networks]
-
-        # Filtrar os nós Wi-Fi com base nos SSIDs das redes
-        queryset = WifiNode.objects.filter(ssid__in=ssids)
-
-        return queryset
+        # Obtém as três redes mais próximas
+        nearest_networks = DeviceLocation.objects.filter(device_id=device_id).order_by('distance')[:3]
+        network_coords_dict = []
+        
+        for nearest_network in nearest_networks:
+            network_x = Networks.objects.filter(bssid=nearest_network.bssid).values_list('x_coord', flat=True)
+            network_y = Networks.objects.filter(bssid=nearest_network.bssid).values_list('y_coord', flat=True)
+            network_coords_dict.append({'bssid': nearest_network.bssid, 'x': network_x[0], 'y': network_y[0], 'distance': nearest_network.distance})
+        
+        if len(network_coords_dict) == 3:
+            x1 = network_coords_dict[0]['x']
+            y1 = network_coords_dict[0]['y']
+            distancia1 = nearest_networks[0].distance
+            x2 = network_coords_dict[1]['x']
+            y2 = network_coords_dict[1]['y']
+            distancia2 = nearest_networks[1].distance
+            x3 = network_coords_dict[2]['x']
+            y3 = network_coords_dict[2]['y']
+            distancia3 = nearest_networks[2].distance
+            x, y = triangulacao(x1, y1, distancia1, x2, y2, distancia2, x3, y3, distancia3)
+            return Response({
+                'used networks': network_coords_dict,
+                'x': x,
+                'y': y
+            })
--- signal_map/signal_map/urls.py
+++ signal_map/signal_map/urls.py
@@ -1,18 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from wifimap.views import WifiNodeViewSet
-from location.views import DeviceLocationViewSet, NearestNetworks, NearestWifiNodes
+from location.views import DeviceLocationViewSet, UserLocation
 from destination.views import ChosenDestinationViewSet
+from networks.views import NetworksViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('wifinodes', WifiNodeViewSet, basename='wifinodes')
 router.register('devicel', DeviceLocationViewSet, basename='devicel')
 router.register('chosendest', ChosenDestinationViewSet, basename='chosendest')
+router.register('networks', NetworksViewSet, basename='networks')
 
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('devicel/<int:device_id>/nearest_networks', NearestNetworks.as_view()),
-    path('devicel/<int:device_id>/nearest_nodes', NearestWifiNodes.as_view()),
+    path('devicel/<int:device_id>/user_location', UserLocation.as_view()),
 ]
